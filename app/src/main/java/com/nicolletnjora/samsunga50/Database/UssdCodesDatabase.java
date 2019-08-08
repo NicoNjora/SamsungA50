81375c093d771ff23835294b997223aa57b97cfe
Philip Wafula <philipwafula2@gmail.com>
2019-08-08 12:02:54 +0300

    Created database instance for ussdcodes

--- app/src/main/java/com/nicolletnjora/samsunga50/Database/UssdCodesDatabase.java
+++ app/src/main/java/com/nicolletnjora/samsunga50/Database/UssdCodesDatabase.java
@@ -1,6 +1,34 @@
 package com.nicolletnjora.samsunga50.Database;
 
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-public class UssdCodesDatabase{
+
+import com.nicolletnjora.samsunga50.Database.Daos.UssdCodeDao;
+import com.nicolletnjora.samsunga50.Models.UssdCode;
+
+
+
+@Database(entities = {UssdCode.class}, version = 1)
+public abstract class UssdCodesDatabase extends RoomDatabase {
+
+    private static UssdCodesDatabase INSTANCE;
+    public abstract UssdCodeDao ussdCodeDao();
+    private static final Object syncLockObject = new Object();
+
+    public static UssdCodesDatabase getDatabaseInstance(Context context){
+        if (INSTANCE == null){
+            synchronized (syncLockObject){
+                INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                        UssdCodesDatabase.class, "UssdCode.db")
+                        .allowMainThreadQueries()
+                        .build();
+            }
+        }
+        return INSTANCE;
+    }
+
 }
