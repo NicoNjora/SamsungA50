d5c1f2b09c5ebd75fabd160afde6719549f5f66d
DavidJonKariz <daivd.j.kariuki@gmail.com>
2019-08-08 15:02:10 +0300

    OCR implementation
    Camera module is opened, but is yet to actually scan the text.

---
+++ app/src/main/java/com/nicolletnjora/samsunga50/Activities/OcrCaptureActivity.java
@@ -0,0 +1,177 @@
+package com.nicolletnjora.samsunga50.Activities;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
+import android.content.Context;
+import android.content.IntentFilter;
+import com.nicolletnjora.samsunga50.R;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
+
+//import android.hardware.Camera;
+//import android.speech.tts.TextToSpeech;
+//import android.view.GestureDetector;
+//import android.view.ScaleGestureDetector;
+
+public class OcrCaptureActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private SurfaceView cameraView;
+    private CameraSource cameraSource;
+
+    private static final String TAG = "OcrCaptureActivity";
+    private static final int requestPermissionID = 101;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ocr_reader_view);
+
+        cameraView = findViewById(R.id.surfaceView);
+        textView = findViewById(R.id.text_view);
+
+        createCameraSource();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode != requestPermissionID) {
+            Log.d(TAG, "Got unexpected permission result: " + requestCode);
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
+        }
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            try {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                cameraSource.start(cameraView.getHolder());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+//    private GraphicOverlay<OcrGraphic> graphicOverlay;
+
+//    private static final int RC_HANDLE_GMS = 9001;
+//    private static final int RC_HANDLE_CAMERA_PERM = 2;
+//    public static final String AutoFocus = "AutoFocus";
+//    public static final String UseFlash = "UseFlash";
+//    public static final String TextBlockObject = "String";
+//    private CameraSourcePreview preview;
+//    private ScaleGestureDetector scaleGestureDetector;
+//    private GestureDetector gestureDetector;
+//    private TextToSpeech tts;
+
+    private void createCameraSource() {
+        Context context = getApplicationContext();
+
+        // Create TextRecognizer
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(context).build();
+        // Set Processor
+//        textRecognizer.setProcessor(new OcrDetectorProcessor(graphicOverlay));
+
+        //Check if TextRecognizer is operational
+        if (!textRecognizer.isOperational()) {
+            Log.w(TAG, "Detector dependencies are not yet available.");
+
+            // ToDo Deprecated Intent for low storage
+            IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+            boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
+
+            if (hasLowStorage) {
+                Toast.makeText(this, R.string.low_storage_error, Toast.LENGTH_LONG).show();
+                Log.w(TAG, getString(R.string.low_storage_error));
+            }
+        }
+
+        // CameraSource
+        cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setRequestedPreviewSize(1280, 1024)
+                .setRequestedFps(2.0f)
+                // ToDO setFlashMode
+                .setAutoFocusEnabled(true)
+                .build();
+
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                            Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                        ActivityCompat.requestPermissions(OcrCaptureActivity.this,
+                                new String[]{Manifest.permission.CAMERA},
+                                requestPermissionID);
+                        return;
+                    }
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        //Set the TextRecognizer's Processor.
+        textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+            @Override
+            public void release() {
+            }
+
+            /**
+             * Detect all the text from camera using TextBlock and the values into a stringBuilder
+             * which will then be set to the textView.
+             */
+            @Override
+            public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                final SparseArray<TextBlock> items = detections.getDetectedItems();
+                if (items.size() != 0) {
+
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            StringBuilder stringBuilder = new StringBuilder();
+                            for (int i = 0; i < items.size(); i++) {
+                                TextBlock item = items.valueAt(i);
+                                stringBuilder.append(item.getValue());
+                                stringBuilder.append("\n");
+                            }
+                            textView.setText(stringBuilder.toString());
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
